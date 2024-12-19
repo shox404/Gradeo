5561fee83364b6f59fdcb950e237b6c366ea9882
shox404 <thesh8080@gmail.com>
2024-12-19 17:24:14 +0500

    Update

--- handlers/view_marks/view_daily_marks.py
+++ handlers/view_marks/view_daily_marks.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from datetime import datetime, timezone
 from firebase.config import db
+from firebase.functions.users import get_user_data
 
 view_daily_marks_router = Router()
 
@@ -19,13 +20,15 @@ async def get_user_role_and_class(user_id: str) -> tuple:
 async def handle_daily_marks(callback: CallbackQuery):
     user_id = str(callback.from_user.id)
     role, user_class_id = await get_user_role_and_class(user_id)
-    print(role,user_class_id)
+
     if not role or not user_class_id:
         await callback.message.edit_text("❌ Unable to retrieve user information.")
         return
 
-    today = datetime.now(timezone.utc).strftime("%Y-%m-%d")
-    print(today)
+    today = datetime.now(timezone.utc).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+
     marks_ref = db.collection("marks")
     query = marks_ref.where("class_id", "==", user_class_id).where(
         "timestamp", ">=", today
@@ -34,20 +37,23 @@ async def handle_daily_marks(callback: CallbackQuery):
         query = query.where("student_id", "==", user_id)
 
     marks_snapshot = query.get()
-    print(marks_snapshot)
 
     if marks_snapshot:
-        marks_text = f"<b>📅 Today's Marks ({today}):</b>\n\n"
+        marks_text = f"<b>📅 Today's Marks ({datetime.now().strftime("%Y-%m-%d")}):</b>\n\n"
         for mark in marks_snapshot:
             mark_data = mark.to_dict()
-            subject_id = mark_data.get("subject_id", "Unknown Subject")
+            teacher_data = await get_user_data(mark_data.get("teacher_id"))
+            subject_id = teacher_data["position"]
             student_name = mark_data.get("student_name", "Unknown Student")
             mark_value = mark_data.get("mark", "N/A")
             timestamp = mark_data.get("timestamp").strftime("%H:%M")
-
             subject_ref = db.collection("subjects").document(subject_id)
             subject_doc = subject_ref.get()
-            subject_name = subject_doc.to_dict().get("name", "Unknown Subject")
+            if subject_doc.exists:
+                subject = subject_doc.to_dict()
+                subject_name = subject.get("name")
+            else:
+                subject_name = "Unknown Subject"
 
             if role == "Student":
                 marks_text += f"📝 {subject_name}: {mark_value} at {timestamp}\n"
